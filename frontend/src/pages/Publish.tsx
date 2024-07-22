e728c5a764272040582b9f39608c80bb59eadf16
mrinal-mann <mrinal.mann24@gmail.com>
2024-07-22 14:04:07 +0530

    change email to title in the input box

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -19,7 +19,7 @@ export const Publish = () => {
             }}
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="Enter your email"
+            placeholder="Title"
           />
           <TextEditor
             onChange={(e) => {
